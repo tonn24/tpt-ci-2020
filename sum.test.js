3af70e91e63cc069ede68308a0aaaf526ed9d518
Siim Tiilen <eritikass@gmail.com>
2020-03-06 12:57:59 +0200

    fix error

--- sum.test.js
+++ sum.test.js
@@ -4,7 +4,7 @@ describe('sum.js', () => {
   describe('normal flow', () => {
     test('sum 1 + 2 equals 3', () => {
       const value = sum(1, 2);
-      expect(value).toBe(5);
+      expect(value).toBe(3);
     });
 
     test('sum 1 + 4 equals 5', () => {
