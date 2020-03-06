48621eafe6ce54c59a7d58bdcf0b295d41450d2d
Siim Tiilen <eritikass@gmail.com>
2020-03-06 12:30:16 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## tpt-ci
+
+follow up for https://github.com/eritikass/kta-19e_tund1
---
+++ sum.js
@@ -0,0 +1,9 @@
+function randonthindgmidagi(a = 0, b = 'kala') {
+  const a1 = Number(a);
+  const b1 = Number(b);
+  if (Number.isNaN(a1) || Number.isNaN(b1)) {
+    throw new Error('Invalid arguments');
+  }
+  return a1 + b1;
+}
+module.exports = randonthindgmidagi;
